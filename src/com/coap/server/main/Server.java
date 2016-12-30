ea4bd07b6187aec0f34f3a1d41f8bebddfc9f0b4
Gianni Pollina <gianni.pollina1@gmail.com>
2016-12-30 01:36:31 +0100

    READ SUBSCRIBE and UNSUBSCRIBE implementation

--- src/com/coap/server/main/Server.java
+++ src/com/coap/server/main/Server.java
@@ -5,14 +5,16 @@ import java.net.SocketException;
  *
  * Version 0.1
  * 
+ * This Coap Broker Accept only the request with content format 40 (application/link-format) 
+ * 
  ******************************************************************************/
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Random;
 
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapServer;
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
-import org.eclipse.californium.core.coap.Response;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 import org.eclipse.californium.core.server.resources.Resource;
 import com.coap.server.main.BrokerUtility;
@@ -52,7 +54,7 @@ public class Server extends CoapServer {
     	/*Constructor*/
 
         // provide an instance of a resource
-        add(new PublishResource("ps","Radice","","","",false).add(new PublishResource("Temp","30 C","temperature-c","sensor","40",true)).add(new PublishResource("ligth","30 lux","ligth-lux","sensor","40",true)));
+        add(new PublishResource("ps","Radice","","","",false).add(new PublishResource("Temp","Temp","temperature-c","sensor","40",true)).add(new PublishResource("ligth","ligth","ligth-lux","sensor","40",true)));
     }
 
     /**
@@ -60,6 +62,7 @@ public class Server extends CoapServer {
      */
     class PublishResource extends CoapResource {
 
+        private int mValue=30; //This is the value of the resource
         
         /**
          * Instantiates a new publish resource.
@@ -73,6 +76,7 @@ public class Server extends CoapServer {
             
             // set display name
             getAttributes().setTitle(title);
+      
             if(rt.length()>0)
             	getAttributes().setAttribute("rt", rt);
             if(if1.length()>0)
@@ -80,6 +84,7 @@ public class Server extends CoapServer {
             if(ct.length()>0)
             	getAttributes().setAttribute("ct", ct);
             if(obs)
+            	setObservable(true);
             	getAttributes().setObservable();
         }
 
@@ -100,10 +105,9 @@ public class Server extends CoapServer {
         @Override
         public void handlePOST(CoapExchange exchange) {  
         	
-        	System.out.println("POST request received");
-        	
-            System.out.println("I send the message"+exchange.getRequestText());          
-            exchange.respond("POST_REQUEST_SUCCESS");           
+
+        	if(exchange.getRequestOptions().hasObserve())
+        	exchange.respond(Integer.toString(getObserverCount()));        
         }
         
         /* 
@@ -112,56 +116,89 @@ public class Server extends CoapServer {
         @Override
         public void handleGET(CoapExchange exchange) {
         	
-        	/*Implement DISCOVER*/
+        	/*
+        	 * N.B. the observing (and then Subscribe-Unsubscribe) is automatically handled by Californium
+        	 * */
         	
         	System.out.println("GET request"
         			+exchange.getRequestOptions().getURIPathString());
         	
-        	//return the number of queries
-        	System.out.println("Query count : "
-        			+exchange.getRequestOptions().getURIQueryCount());
+        	String path=exchange.getRequestOptions().getURIPathString();
         	
-        	int nQueries=exchange.getRequestOptions().getURIQueryCount();
-        	
-        	for(int i=0;i<nQueries;i++){
-	        
-        		//return the list of queries
-	        	System.out.println("Query-"+i+" : "
-	        			+exchange.getRequestOptions().getURIQueries().get(i));
+        	if(path.equals("ps/")||path.equals("ps")){
+
+            	/**-----Implement DISCOVER------**/
+            	
+        		
+        		/*This is the DISCOVER because the path request has form ps or ps/*/
+        		
+        		//return the number of queries
+	        	System.out.println("Query count : "
+	        			+exchange.getRequestOptions().getURIQueryCount());
+	        	
+	        	int nQueries=exchange.getRequestOptions().getURIQueryCount();
+	        	
+	        	for(int i=0;i<nQueries;i++){
+		        
+	        		//return the list of queries
+		        	System.out.println("Query-"+i+" : "
+		        			+exchange.getRequestOptions().getURIQueries().get(i));
+		        }
+	        	
+	        	//I want the list of child
+	        	Collection<Resource> listOfChild=getChildren();
+	        	
+	        	Iterator<Resource> i=listOfChild.iterator();
+	        	
+	        	String res=null;
+	        	String str=null;
+	        	StringBuilder builder=new StringBuilder();//this is a builder used to concatenate multiple string
+	        	
+	        	//scroll all child resources founded
+	        	while(i.hasNext()){
+	        	
+	        		Resource r=i.next();
+	
+	        		res=BrokerUtility.buildResponce(r,nQueries,exchange.getRequestOptions().getURIQueries());
+	        		
+	        		if(res!=null){
+	        			builder.append(res).append("\n");
+	        		}
+	        		
+	        	}
+	        	
+	        	if(res!=null)
+	        		str=builder.toString();
+	    		
+	        	exchange.respond(str==null?ResponseCode.NOT_FOUND:ResponseCode.CONTENT,str==null?"":str);            
 	        }
-        	
-        	//I want the list of child
-        	Collection<Resource> listOfChild=getChildren();
-        	
-        	Iterator<Resource> i=listOfChild.iterator();
-        	
-        	String res=null;
-        	String str=null;
-        	StringBuilder builder=new StringBuilder();//this is a builder used to concatenate multiple string
-        	
-        	//scroll all child resources founded
-        	while(i.hasNext()){
-        	
-        		Resource r=i.next();
+        	else{
+        		/**-----Implement READ*----*/
+        		String value=new String(getAttributes().getAttributeValues("value").get(0));
+        			
 
-        		res=BrokerUtility.buildResponce(r,nQueries,exchange.getRequestOptions().getURIQueries());
-        		
-        		if(res!=null){
-        			builder.append(res).append("\n");
+        		if(exchange.getRequestOptions().getContentFormat()!=40){
+        				//I accept only the content type 40 ==> application/link-format
+        				exchange.respond(ResponseCode.UNSUPPORTED_CONTENT_FORMAT,"");
         		}
-        		
+        		else{
+	        		//N.B. In case of not found resource Californium automatically set the Code 4.04
+	        		
+	        		//If the value is not included i return REQUEST_ENTITY_INCOMPLETE code
+	        		exchange.respond(value.length()==0?ResponseCode.REQUEST_ENTITY_INCOMPLETE:ResponseCode.CONTENT,Integer.toString(mValue));
+        		}
         	}
-
-        	str=builder.toString();
-    		exchange.respond(str==null?ResponseCode.NOT_FOUND:ResponseCode.CONTENT,str==null?"":str);            
-    		
         }
         
         
         public void handlePUT(CoapExchange exchange) {
-        	 
-        	exchange.respond("PUT");
-        	changed(); // notify all observers
+        	
+        	//This is a test value
+        	mValue=new Random().nextInt();
+        	//exchange.respond(ResponseCode.CHANGED);
+        	
+        		changed(); // notify all observers
+     
         }
         	  
         
